42fc397b529d57cd302c4101e8ddf7e50ddb2d62
marinasupernova <marina.de.suvorova@gmail.com>
2021-08-25 19:59:57 +0200

    updated datetime lines of code

--- tables_reservations/views.py
+++ tables_reservations/views.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime 
+import datetime 
 from django.shortcuts import render
 from tables_reservations.models import Table, Reservation
 from django.core.exceptions import ObjectDoesNotExist
@@ -9,7 +9,7 @@ def display_tables(request):
     if request.GET and request.GET['date_reservation']:
         date_reservation = request.GET['date_reservation']
     else:
-        date_reservation = str(date.today())
+        date_reservation = str(datetime.date.today())
 
 
     if request.POST and request.POST['table_number']:
@@ -31,12 +31,15 @@ def display_tables(request):
         for table in tables:
             if table.number == reservation.table_number:
                 table.reserved_status = True
-    year, month, day = date_reservation.split("-")
+
     context = {
         'tables': tables,
         'current_date': date_reservation,
-        #'previous_day': (datetime.date(int(year), int(month), int(day)) - datetime.timedelta(1)).isoformat(),
-        #'next_day': (datetime.date(int(year), int(month), int(day)) + datetime.timedelta(1)).isoformat()
+        'previous_date': (datetime.datetime.fromisoformat(date_reservation) - datetime.timedelta(1)).isoformat()[:-9],
+        'next_date':     (datetime.datetime.fromisoformat(date_reservation) + datetime.timedelta(1)).isoformat()[:-9]
     }
 
     return render(request, 'table_reservations.html', context)
+
+# 2021-08-23T00:00:00 => 2021-08-23
+# str[:-8]
